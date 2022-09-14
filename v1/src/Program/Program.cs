6840a2d3b38075e1211e84c9f5a690d990668a42
FranciscoNasso <francisco.nasso@correo.ucu.edu.uy>
2022-09-14 10:35:18 -0300

    Ejercicio v1 terminado, creo

--- v1/src/Program/Program.cs
+++ v1/src/Program/Program.cs
@@ -7,17 +7,11 @@ namespace Person
     {
         static void Main(string[] args)
         {
-            Person tota;
-            tota = new Person("Diego", "Lugano");
-            Console.WriteLine($"La 'Tota' {tota.Name} {tota.FamilyName}");
-            string json = JsonSerializer.Serialize<Person>(tota);
-            Console.WriteLine(json);
+            Person person = new Person("Diego", "Lugano");
+            Console.WriteLine(person.ConvertToJson());
 
-            Person faraon;
-            faraon = JsonSerializer.Deserialize<Person>("{\"Name\":\"Diego\",\"FamilyName\":\"Godín\"}");
-            Console.WriteLine($"El 'Faraón' {faraon.Name} {tota.FamilyName}");
-            json = JsonSerializer.Serialize(faraon);
-            Console.WriteLine(json);
+            Person person1 = new Person("{\"Name\":\"Diego\",\"FamilyName\":\"Godín\"}");
+            Console.WriteLine(person.ConvertToJson());
         }
     }
 }
